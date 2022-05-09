a03ad5bb6fe050042f2859b714e30d8b3fae5cc4
yogeshmathur <yogesh.mathur@nagarro.com>
2022-05-09 17:59:19 +0530

    no message

--- 09-05-22/quicksort.py
+++ 09-05-22/quicksort.py
@@ -6,7 +6,8 @@ def quick_sort(arr: List[int]) -> None:
     
 def quick_sort_helper(arr: List[int], left, right) -> None:
     if left < right:
-        splitting_idx = partition(arr, left, right)
+        # splitting_idx = partition(arr, left, right)
+        splitting_idx = hoare_partition(arr, left, right)
         quick_sort_helper(arr, left, splitting_idx - 1)
         quick_sort_helper(arr, splitting_idx + 1, right)
 
@@ -21,6 +22,20 @@ def partition(arr: List[int], left: int, right: int) -> None:
     i += 1
     swap(i, right, arr)
     return i
+
+def hoare_partition(arr: List[int], left: int, right: int) -> None:
+    pivot = arr[left]
+    pivot_idx = left
+    left += 1
+    while left <= right:
+        if arr[left] > pivot and arr[right] < pivot:
+            swap(left, right, arr)
+        if arr[left] <= pivot:
+            left += 1
+        if arr[right] >= pivot:
+            right -= 1
+    swap(pivot_idx, right, arr)
+    return right
 
 def swap(left: int, right: int, arr: List[int]) -> None:
     arr[left], arr[right] = arr[right], arr[left]
