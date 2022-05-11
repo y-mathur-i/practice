ccb8afea9f402eb8d4e50eabd1d86f10b44fa8a6
yogeshmathur <yogesh.mathur@nagarro.com>
2022-05-11 14:41:18 +0530

    updated count sort

--- 11-05-22/count_sort.py
+++ 11-05-22/count_sort.py
@@ -1,5 +1,5 @@
 from typing import List
-from random import randint
+from random import randint, shuffle
 
 def countSort(arr: List[int]) -> None:
     min_ele = min(arr)
@@ -8,14 +8,24 @@ def countSort(arr: List[int]) -> None:
     freq_arr = [0 for _ in range(len_freq_arr)]
     for n in arr:
         freq_arr[n-min_ele] += 1
-    i = 0
-    for idx, freq in enumerate(freq_arr):
-        for _ in range(freq):
-            arr[i] = min_ele + idx
+    for i in range(1, len(freq_arr)):
+        freq_arr[i] += freq_arr[i-1]
+    i = j = 0
+    while i < len(arr):
+        while i != freq_arr[j]:
+            arr[i] = j + min_ele
             i += 1
+        j += 1
+    # i = 0
+    # for idx, freq in enumerate(freq_arr):
+    #     for _ in range(freq):
+    #         arr[i] = min_ele + idx
+    #         i += 1
     return arr
 
 arr = [randint(1, 10) for _ in range(10)]
+arr = list(range(10))
+shuffle(arr)
 print(arr)
 countSort(arr)
 print(arr)
