6a15a84415f5eb1b8b71eb518259a171fe6a0911
yogeshmathur <yogesh.mathur@nagarro.com>
2022-05-13 13:27:37 +0530

    update

--- 13-05-22/topological_sort_dfs.py
+++ 13-05-22/topological_sort_dfs.py
@@ -5,10 +5,9 @@ from typing import List
 This is an example based on a problem on leetcode
 problem no 2192
 -- all ancestors of a node in a DAG
-        I found topologically sorted order of vertices and then 
-        did dfs for each node on the graph based on that order
+    --- not really topo sort 
+        had to reversed the graph and find all the ancestors
 """
-
 
 class Vertex:
     def __init__(self):
@@ -22,29 +21,25 @@ class Solution:
         graph = {vertex: Vertex() for vertex in range(n)}
         rev_graph = {vertex:[] for vertex in range(n)}
         for source, dest in edges:
-            graph[source].neighbours.append(dest)
             rev_graph[dest].append(source)
 
-        for i in range(n):
-            if i not in self.visited:
-                self.__topo_sort(graph, i)
         parents = {}
-        for v in self.res:
+        for v in range(n):
             done = set()
             self.__dfs(rev_graph, done, v)
             done.remove(v)
             parents[v] = sorted(done)
         return list(parents[k] for k in sorted(parents.keys()))
-
-    def __topo_sort(self, graph, curr):
-        for nei in graph[curr].neighbours:
-            if nei not in self.visited:
-                self.__topo_sort(graph, nei)
-        self.visited.add(curr)
-        self.res.append(curr)
     
     def __dfs(self, graph, done, curr):
         if curr not in done:
             done.add(curr)
             for nei in graph[curr]:
                 self.__dfs(graph, done, nei)
+        
+        
+        
+        
+        
+        
+        
